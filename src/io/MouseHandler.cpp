f5bd569f44d970436fee9fd13fca7df651c3c506
eshahrabani <shahrabani.elan@gmail.com>
2016-08-06 23:33:19 -0700

    Current-functionality implementation of MouseHandler.

--- src/io/MouseHandler.cpp
+++ src/io/MouseHandler.cpp
@@ -4,3 +4,50 @@
 MouseHandler::MouseHandler(ofApp* app) {
 	this->app = app;
 }
+
+void MouseHandler::handleMoved(int x, int y) {
+	pos_x = x;
+	pos_y = y;
+}
+
+void MouseHandler::handleDragged(int x, int y, int button) {
+	// Handle the mouse movement first. 
+	handleMoved(x, y);
+
+	// If control is pressed, draw a marquee.
+	if (button == OF_MOUSE_BUTTON_LEFT && app->keyHandler->ctrl_pressed && pressed_inside_player) {
+
+		// Assuming 45 degree angles (square marquee): w = h = (d/2)*sqrt(2)
+		float d = sqrt(pow(x - last_clicked_x, 2) + pow(y - last_clicked_y, 2));	// Apply distance formula.
+		int w, h;
+		w = h = (d / 2)*sqrt(2);
+
+		// Set new marquee.
+		if (app->video_player.isLoaded()) {
+			app->marquee.set(last_clicked_x, last_clicked_y, w, h);
+		}
+	}
+}
+
+void MouseHandler::handlePressed(int x, int y, int button) {
+	last_clicked_x = x;
+	last_clicked_y = y;
+	last_clicked_button = button;
+
+	if (button == OF_MOUSE_BUTTON_LEFT) {
+
+		// TODO: Needs streamline via custom videoplayer class. 
+		ofRectangle area;
+		area.set(app->vid_x, app->vid_y, app->vid_width, app->vid_height);
+
+		if (area.inside(x, y)) {
+			pressed_inside_player = true;
+		}
+	}
+}
+
+void MouseHandler::handleReleased(int x, int y, int button) {
+	if (button == OF_MOUSE_BUTTON_LEFT && pressed_inside_player) {
+		pressed_inside_player = false;
+	}
+}
